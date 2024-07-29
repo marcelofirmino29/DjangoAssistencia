a06401d1523bfefb4d0016c3cfd2b1e2999c8df5
marcelofirmino29 <marc.firmino@gmail.com>
2024-07-29 17:17:24 -0400

    CRIAÇÃO DE MODELOS

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Embalagem)
+admin.site.register(TipoSabor)
+admin.site.register(Sabor)
+admin.site.register(SelSabor)
+admin.site.register(Cobertura)
+admin.site.register(MontaPote)
+admin.site.register(SacolaItens)
+admin.site.register(Pedido)

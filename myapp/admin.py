3bddae6d58308b3e1c97a240b4a4d72ab74d8603
marcelofirmino29 <marc.firmino@gmail.com>
2024-07-30 13:01:22 -0400

    Customizações em modelos e admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,11 +1,35 @@
 from django.contrib import admin
-from .models import *
+from myapp import models
 
-admin.site.register(Embalagem)
-admin.site.register(TipoSabor)
-admin.site.register(Sabor)
-admin.site.register(SelSabor)
-admin.site.register(Cobertura)
-admin.site.register(MontaPote)
-admin.site.register(SacolaItens)
-admin.site.register(Pedido)
+# Registre seus modelos aqui
+admin.site.register(models.Embalagem)
+admin.site.register(models.TipoSabor)
+admin.site.register(models.Sabor)
+admin.site.register(models.Cobertura)
+admin.site.register(models.Pedido)
+
+# Monta Pote
+class SelSaborInline(admin.TabularInline):
+    model = models.SelSabor
+    extra = 0
+
+@admin.register(models.MontaPote)
+class MontaPoteAdmin(admin.ModelAdmin):
+    inlines = [
+        SelSaborInline
+    ] 
+
+# Sacola de Itens
+class MontaPoteInline(admin.TabularInline):
+    model = models.SacolaItens.potes.through
+    extra = 0
+
+class PedidoInline(admin.StackedInline):
+    model = models.Pedido
+    extra = 0
+    
+@admin.register(models.SacolaItens)
+class SacolaItensAdmin(admin.ModelAdmin):
+    fields = ('preco',)
+    readonly_fields = ('preco',)  # Adicione o campo readonly para mostrar o preço total
+    inlines = [PedidoInline,MontaPoteInline]
--- myapp/models.py
+++ myapp/models.py
@@ -64,14 +64,27 @@ class Cobertura(models.Model):
     class Meta:
         verbose_name = '4 - Cobertura'
         verbose_name_plural = '4 - Cobertura' 
-    # Monta o Pote  
+
+# Monta o Pote  
 class MontaPote(models.Model):
     embalagem = models.ForeignKey(Embalagem, related_name='embalagem', on_delete=models.CASCADE, null=True)
     coberturas = models.ManyToManyField(Cobertura)
-    quantidade = models.PositiveIntegerField(null=True) 
+    quantidade = models.PositiveIntegerField(null=True)
+    
+    def preco_total(self):
+        preco_embalagem = self.embalagem.preco if self.embalagem else 0
+        preco_coberturas = sum(cobertura.preco for cobertura in self.coberturas.all())
+        preco_sabores = 0
+        for selsabor in self.pote.all():
+            preco_sabor = selsabor.sabor.tipo.preco
+            quantidade_bolas = selsabor.quantidade_bolas
+            preco_sabores += preco_sabor * quantidade_bolas
+        total_pote = preco_embalagem + preco_coberturas + preco_sabores
+        total = total_pote * self.quantidade
+        return total 
 
     def __str__(self):
-        return f"ID: {self.id} / POTE: {self.embalagem.tipo} / Qtd: {self.quantidade}"
+        return f"ID: {self.id} / POTE: {self.embalagem.tipo} / Qtd: {self.quantidade} / {self.preco_total()}"
 
     class Meta:
         verbose_name = 'B - MontaPote'
@@ -90,16 +103,25 @@ class SelSabor(models.Model):
         verbose_name = 'A - SelSabor'
         verbose_name_plural = 'A - SelSabor'
 
-    # Sacolas de Itens
+# Sacolas de Itens
 class SacolaItens(models.Model):
     potes = models.ManyToManyField(MontaPote)
-    preco = models.DecimalField(max_digits=10, decimal_places=2, null=True)  # Armazena o valor como um número decimal
-    
+    preco = models.DecimalField(max_digits=10, decimal_places=2)  # Armazena o valor como um número decimal
+  
     def preco_formatado(self):
         return f'R$ {self.preco:.2f}'  # Formata o valor com 2 casas decimais
-    
+
+    def preco_total(self):
+        # Calcule a soma dos preços de todos os potes na sacola
+        sacola_total = 0
+        for pote in self.potes.all():
+            sacola_total += pote.preco_total()
+        self.preco = sacola_total
+        self.save()
+        return sacola_total
+        
     def __str__(self):
-        return f"CARRINHO: {self.id}"
+        return f"CARINHO: {self.id} / {self.preco_total()}"
 
     class Meta:
         verbose_name = 'C - SacolaItens'
